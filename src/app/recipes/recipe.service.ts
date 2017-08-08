3d83d3865fe2600a2de1cf3c97ac5507ba2bbe0b
Mike Field-May <mfieldmay@gmail.com>
2017-08-08 15:31:21 +0100

    Update recipe model to contain ingredients

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,16 @@
 import {EventEmitter} from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
     private recipes: Recipe[] = [
     new Recipe('Fish and Chips',
                 'Super tasty supper',
-                'http://www.bizzielizzies.co.uk/wp-content/uploads/2015/03/slide-fish-chips-2015.jpg')
+                'http://www.bizzielizzies.co.uk/wp-content/uploads/2015/03/slide-fish-chips-2015.jpg',
+                [
+                  new Ingredient('Fish', 1),
+                  new Ingredient('Chips', 20)
+                ])
   ];
   recipeSelected = new EventEmitter<Recipe>();
 
