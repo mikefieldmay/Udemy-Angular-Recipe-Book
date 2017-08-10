9b0d603778df2d19f918fe5c9aa929055cf1bd7d
Mike Field-May <mfieldmay@gmail.com>
2017-08-10 18:17:38 +0100

    Create edit component and route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,15 @@ import {AppComponent} from './app.component';
 import {RouterModule, Routes} from '@angular/router';
 import { NgModule } from '@angular/core';
 import { RecipeStartComponent } from 'app/recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from 'app/recipes/recipe-edit/recipe-edit.component';
 
 const AppRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   { path: 'recipes', component: RecipesComponent, children: [
     { path: '', component: RecipeStartComponent},
-     { path: ':id', component: RecipeDetailComponent}
+    { path: 'new', component: RecipeEditComponent},
+    { path: ':id', component: RecipeDetailComponent},
+    { path: ':id/edit', component: RecipeEditComponent}
   ] },
   { path: 'shopping-list', component: ShoppingListComponent}
 ];
